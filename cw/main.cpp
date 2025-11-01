4b5e689ecf8148554fbd5e0fccd4ad6016930048
kuzRK <sanalsogdanov11188.com>
2025-11-01 09:33:23 +0000

    Main Part

--- cw/main.cpp
+++ cw/main.cpp
@@ -1,4 +1,29 @@
 #include <iostream>
 
+int make(int rows, int cols)
+{
+
+}
+
+void output(const int *const*mtx)
+{
+
+}
+
+void rm(int **mtx)
+{
+
+}
 int main()
-{}
+{
+  int rows = 0;
+  int cols = 0;
+  std::cin >> rows >> cols;
+  if (std::cin.fail()) {
+    return 1;
+  }
+  int **mtx = nullptr;
+  mtx = make(rows,cols);
+  output(mtx);
+  rm(mtx);
+}
